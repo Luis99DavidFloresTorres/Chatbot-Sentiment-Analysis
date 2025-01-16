7155a84bbcc7a539424fccec84fbf73af54ed573
Luis99DavidFloresTorres <luisdavid99florestorres@gmail.com>
2025-01-15 23:10:57 -0400

    add upload download functions

--- scripts/feed_back_model/pre-process.py
+++ scripts/feed_back_model/pre-process.py
@@ -2,6 +2,7 @@ import pandas as pd
 import boto3
 class PreProcess:
     def __init__(self):
+        print("funciona")
     def upload(self, local_file_path, s3_filename, bucket_name):
 
         s3 = boto3.client('s3')
@@ -13,11 +14,14 @@ class PreProcess:
     def download(self, bucket_name, s3_file_name, local_file_path):
         s3 = boto3.client('s3')
         try:
-            df = s3.download_file(bucket_name,s3_file_name, local_file_path)
-            print(df)
+            s3.download_file(bucket_name,s3_file_name, local_file_path)
             print(f"file {local_file_path} download success in {bucket_name}/{s3_file_name}")
         except Exception as e:
             print(f"Error to download the file: {e}")
-        return pd.read_csv(df)
-    def processText(self, data):
-        print(data)
+    def processText(self, bucket_name,s3_file_name,local_file_path):
+
+        self.download(bucket_name,s3_file_name,local_file_path)
+if __name__ == '__main__':
+    PreProcess = PreProcess()
+    PreProcess.download("mlopsluis","dataset/chatbotSentiment.csv","../../dataset_empathetic/traindownload.csv")
+    #PreProcess.upload("E:/Chatbot Sentiment Analysis/Chatbot-Sentiment-Analysis/dataset_empathetic/empatheticdialogues/train1.csv","dataset/chatbotSentiment.csv","mlopsluis")
