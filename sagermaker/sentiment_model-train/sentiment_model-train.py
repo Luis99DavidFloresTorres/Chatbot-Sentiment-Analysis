a578f94abbf9d817da98874247cdb99619cb29d6
Luis99DavidFloresTorres <luisdavid99florestorres@gmail.com>
2025-01-31 18:17:09 -0400

    update source s3

--- sagermaker/sentiment_model-train/sentiment_model-train.py
+++ sagermaker/sentiment_model-train/sentiment_model-train.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
 
     last_training_job = huggingface_estimator.latest_training_job.name
     source_bucket = bucket_name
-    source_key = f"outputSentimentModel/{last_training_job}/output/model.tar.gz"
+    CopySource={'Bucket': 'mlopsluis', 'Key': f"outputSentimentModel/{last_training_job}/output/model.tar.gz"},
     destination_key = "outputSentimentModel/latest-model.tar.gz"
 
     s3.copy_object(
