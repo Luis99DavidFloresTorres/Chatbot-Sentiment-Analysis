de03e4d97a327a112a6be768b783b8d72b5e2a07
Luis99DavidFloresTorres <luisdavid99florestorres@gmail.com>
2025-01-31 02:02:55 -0400

    update class

--- uploadDataset.py
+++ uploadDataset.py
@@ -1,4 +1,5 @@
 from scripts.feed_back_model.preprocess import PreProcess
 
 if __name__ == '__main__':
-    PreProcess.upload('train_dataset.csv','dataset/train_dataset.csv','mlopsluis')
+    preprocess = PreProcess()
+    preprocess.upload('train_dataset.csv','dataset/train_dataset.csv','mlopsluis')
